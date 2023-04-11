bf99699f8eaea3d25bf78638115513af64e82d15
Angelena Nunez <angelenacorral@gmail.com>
2023-04-11 13:11:39 -0600

    changed app name to display hundred

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ function displayMessage() {
     Swal.fire(
         {
             backdrop: false,
-            title: 'App Name',
+            title: 'Display Hundred',
             text: msg
         }
     );
